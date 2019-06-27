63e3455f23bbf9e7d087fef09bc27398b5d16c63
mathgaming <mhsv16@student.aau.dk>
2019-06-27 15:12:09 +0200

    Fixed AAURedirector

---
+++ ITSWebMgmt/Models/ChangeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITSWebMgmt.Models
+{
+    public class ChangeModel
+    {
+        public string changeID;
+        public string Name;
+        public string Discription;
+        public string Start;
+        public string End;
+        public string Resposeble;
+        public string Error;
+    }
+}
