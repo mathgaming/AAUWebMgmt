fa07e8b26a5de14e046a4f1a43acd91346529fc0
mathgaming <mhsv16@student.aau.dk>
2019-09-03 11:20:34 +0200

    Fixed error page

--- ITSWebMgmt/Startup.cs
+++ ITSWebMgmt/Startup.cs
@@ -48,7 +48,7 @@ namespace ITSWebMgmt
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
