b6704669d999f913962dc5c411a398696306804a
Laurits Brøcker <lbrack17@its.aau.dk>
2019-10-03 15:43:02 +0200

    Created first HTML/CSS setup

---
+++ ITSWebMgmt/Controllers/LIMCController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ITSWebMgmt.Models;
+using ITSWebMgmt.Models.Log;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITSWebMgmt.Controllers
+{
+    public class LIMCController : WebMgmtController
+    {
+        //LIMC stands for LIMC IDM to Mail Converter
+        public LIMCController(LogEntryContext context) : base(context) { }
+
+         public IActionResult Index()
+        {
+            LIMCModel limcModel = new LIMCModel();
+            return View(limcModel);
+        }
+
+    }
+}
---
+++ ITSWebMgmt/Models/LIMCModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITSWebMgmt.Models
+{
+    public class LIMCModel
+    {
+        string RawCSVInput;
+        string MailOutput;
+        public string RawCSVInput1 { get => RawCSVInput; set => RawCSVInput = value; }
+        public string MailOutput1 { get => MailOutput; set => MailOutput = value; }
+    }
+}
