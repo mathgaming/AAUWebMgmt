5958c0e536202480bdc3243070ea37a867fcf77f
mathgaming <mhsv16@student.aau.dk>
2019-10-10 15:58:44 +0200

    Added radio buttons to select convert method (not working)

--- ITSWebMgmt/Controllers/LIMCController.cs
+++ ITSWebMgmt/Controllers/LIMCController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using ITSWebMgmt.Models;
 using ITSWebMgmt.Models.Log;
@@ -34,10 +35,36 @@ namespace ITSWebMgmt.Controllers
                 }
                 UserModel userModel = new UserModel(username, false);
                 List<string> emails = userModel.getUserMails();
-                outputString += String.Join(",", emails);
+                outputString += string.Join(",", emails);
             }
             return Success(outputString);
         }
 
+        public ActionResult JamfConvert([FromBody]string inputCSV)
+        {
+            List<string> lines = inputCSV.Split('\n').ToList();
+            List<string> headers = lines[0].Split(',').ToList();
+
+            int emailIndex = headers.IndexOf("Email Address");
+            int secundaryEmailIndex = headers.IndexOf("AAU-1x Username");
+            int aauNumberIndex = headers.IndexOf("Asset Tag");
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("Email;AAUNumber;Navn\n");
+
+            foreach (var line in lines.Skip(1))
+            {
+                List<string> columbs = line.Split(',').ToList();
+                string email = columbs[emailIndex];
+                if (email != "")
+                {
+                    email = columbs[secundaryEmailIndex];
+                }
+                string name = new UserModel(email, false).DisplayName;
+                sb.Append($"{email};{columbs[aauNumberIndex]};{name}\n");
+            }
+
+            return Success(sb.ToString());
+        }
     }
 }
