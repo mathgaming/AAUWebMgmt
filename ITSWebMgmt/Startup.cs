72613c75a2f8244f2c6ea48e8eb307faf769753a
Laurits Brøcker <lbrack17@its.aau.dk>
2019-10-17 15:29:42 +0200

    Fixed an issue where dotnet would not recompile front end

--- ITSWebMgmt/Startup.cs
+++ ITSWebMgmt/Startup.cs
@@ -24,7 +24,7 @@ namespace ITSWebMgmt
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddMemoryCache();
 
             services.Configure<CookiePolicyOptions>(options =>
