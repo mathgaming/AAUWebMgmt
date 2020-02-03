a2bd978694ccd9124e2f902a61e8743b5ef9ea00
mathgaming <mhsv16@student.aau.dk>
2020-02-03 11:53:05 +0100

    Added model class for log statistics

---
+++ ITSWebMgmt/Models/Log/LogCountStatisticsModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITSWebMgmt.Models.Log
+{
+    public class LogCountStatisticsModel
+    {
+        public string Name { get; set; }
+        public string FromDate { get; set; }
+        public int Count { get; set; }
+
+        public LogCountStatisticsModel(string name, int count, string fromDate = "")
+        {
+            Name = name;
+            Count = count;
+            FromDate = fromDate;
+        }
+    }
+}
