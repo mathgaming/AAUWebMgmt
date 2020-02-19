dbca01a3b8ee43d25c2478e945f8598b65e73c9f
mathgaming <mhsv16@student.aau.dk>
2020-02-19 12:58:43 +0100

    Fix print correctly

--- ITSWebMgmt/Models/PrintModel.cs
+++ ITSWebMgmt/Models/PrintModel.cs
@@ -12,7 +12,7 @@ namespace ITSWebMgmt.Models
         public string AAUCardKonica { get; set; }
         public string DepartmentThing { get; set; }
         public string DepartmentName { get; set; }
-        public string EquitracDisabled { get; set; }
+        public string EquitracDisabled { get; set; } = "";
         public string ConectionError { get; set; }
         public string CredentialError { get; set; }
         public decimal Free { get; set; }
