382739a14773c9d3f03c903c588ea2f781688ae5
mathgaming <mhsv16@student.aau.dk>
2020-02-26 14:16:18 +0100

    Kode til lave liste

---
+++ ITSWebMgmt/Helpers/FindComputersForAllStaff.cs
@@ -0,0 +1,146 @@
+﻿using ITSWebMgmt.Caches;
+using ITSWebMgmt.Connectors;
+using ITSWebMgmt.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ITSWebMgmt.Helpers
+{
+    public class FindComputersForAllStaff
+    {
+        private string filename = @"C:\webmgmtlog\testlist\computer-list.txt";
+
+        public FindComputersForAllStaff()
+        {
+            CombineLists();
+        }
+
+        public void CombineLists()
+        {
+            int batchId = 0;
+            List<List<string>> batches = Readbatches();
+            string outFilename = @"C:\webmgmtlog\testlist\computer-list-full.txt";
+            using StreamWriter outFile = new StreamWriter(outFilename);
+
+            foreach (var batch in batches)
+            {
+                string batchFilename = @"C:\webmgmtlog\testlist\computer-list-" + batchId + ".txt";
+                StreamReader file = new StreamReader(batchFilename);
+                string line;
+                int count = 0;
+                while ((line = file.ReadLine()) != null)
+                {
+                    outFile.WriteLine(line);
+                    count++;
+                }
+
+                if (count != batch.Count)
+                {
+                    Console.WriteLine($"Error in batch {batchId}");
+                }
+
+                batchId++;
+            }
+        }
+
+        private List<List<string>> Readbatches()
+        {
+            List<List<string>> batches = new List<List<string>>();
+            StreamReader file = new StreamReader(filename);
+            string line;
+            while ((line = file.ReadLine()) != null)
+            {
+                batches.Add(line.Split(";").ToList());
+            }
+
+            return batches;
+        }
+
+        public void MakeList()
+        {
+            string group = "LDAP://CN=Aau-staff,OU=Email,OU=Groups,DC=aau,DC=dk";
+            GroupADcache ad = new GroupADcache(group);
+            var membersADPath = ad.getGroupsTransitive("member");
+            List<List<string>> batches = new List<List<string>>();
+            int count = 0;
+            List<string> batch = new List<string>();
+            if (File.Exists(filename))
+            {
+                batches = Readbatches();
+            }
+            else
+            {
+                using StreamWriter file = new StreamWriter(filename);
+                foreach (var adpath in membersADPath)
+                {
+                    if (adpath.Contains("OU=People") || adpath.Contains("OU=Admin Identities"))
+                    {
+                        if (count == 100)
+                        {
+                            file.WriteLine(String.Join(";", batch));
+                            batches.Add(batch);
+                            batch = new List<string>();
+                            count = 0;
+                        }
+                        batch.Add(adpath);
+                        count++;
+                    }
+                }
+            }
+
+            count = 0;
+
+            foreach (var b in batches)
+            {
+                RunBatch(b, count);
+                count++;
+            }
+        }
+
+        public void RunBatch(List<string> adpaths, int batch)
+        {
+            string batchFilename = @"C:\webmgmtlog\testlist\computer-list-" + batch + ".txt";
+            try
+            {
+                if (!File.Exists(batchFilename))
+                {
+                    using StreamWriter file = new StreamWriter(batchFilename);
+                    foreach (var adpath in adpaths)
+                    {
+                        var split = adpath.Split(',');
+                        var name = split[0].Replace("CN=", "");
+                        var domain = split.Where(s => s.StartsWith("DC=")).ToArray()[0].Replace("DC=", "");
+                        var upn = $"{name}@{domain}.aau.dk";
+
+                        UserModel model = new UserModel(adpath, "");
+                        List<string> windowsNames = new List<string>();
+                        string formattedName = string.Format("{0}\\\\{1}", domain, name);
+
+                        foreach (ManagementObject o in model.getUserMachineRelationshipFromUserName(formattedName))
+                        {
+                            windowsNames.Add(o.Properties["ResourceName"].Value.ToString());
+                        }
+
+                        JamfConnector jamf = new JamfConnector();
+                        List<string> macComputers = jamf.getComputerNamesForUser(upn);
+
+                        string line = $"{upn};{String.Join(",", windowsNames)};{String.Join(",", macComputers)}";
+                        file.WriteLine(line);
+
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
