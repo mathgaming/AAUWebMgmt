3dbdd32353020b23b3a1b9697c62c41ddfdfe961
mathgaming <mhsv16@student.aau.dk>
2020-08-18 15:36:10 +0200

    Added jamf email creator

---
+++ ITSWebMgmt/Models/JamfGroupModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITSWebMgmt.Models
+{
+    public class JamfGroupModel
+    {
+    }
+
+    public class AdvancedComputerSearch
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class AdvancedComputerSearchList
+    {
+        public List<AdvancedComputerSearch> advanced_computer_searches { get; set; }
+    }
+}
